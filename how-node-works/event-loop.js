45d88e08cea22c9884760b0aee2f4579fbe9b3ea
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-14 12:30:01 +0500

    Add events

--- how-node-works/event-loop.js
+++ how-node-works/event-loop.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const start = Date.now();
 
 // process.env.UV_THREADPOOL_SIZE = 1;  // same time 1 process
+process.env.UV_THREADPOOL_SIZE = 4;
 
 setTimeout(() => console.log('timer 1 finished'), 0 );
 
@@ -26,6 +27,14 @@ fs.readFile('test-file.txt', () => {
   crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512')
   console.log(Date.now() - start, "Password encrypted 1");
   
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512')
+  console.log(Date.now() - start, "Password encrypted 2");
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512')
+  console.log(Date.now() - start, "Password encrypted 3");
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512')
+  console.log(Date.now() - start, "Password encrypted 4");
 })
 
 console.log('Hello from the top-level code');
@@ -80,3 +89,33 @@ console.log('Hello from the top-level code');
 // Immediate 2 finished
 // timer 2 finished
 // timer 3 finished
+
+// 6
+// Hello from the top-level code
+// timer 1 finished
+// Immediate 1 finished
+// I/O finished
+// -------------------
+// 942 Password encrypted 1  // ! sync 4
+// 2018 Password encrypted 2
+// 2950 Password encrypted 3
+// 3886 Password encrypted 4
+// Process.nextTick
+// Immediate 2 finished
+// timer 2 finished
+// timer 3 finished
+
+
+
+// 34. Events and Event-Driven Architecture
+
+// |          OBSERVER PATTERN            |     
+// | Event Emitter   -->   Event Listener |   -->   Attached callback function
+// |          emit events                 |  Calls
+
+
+// emitter                listener              request received
+// 127.0.0.1:8000         server.on(...)
+                       // instance of EventEmitter class
+
+// 35. Events in Practice
