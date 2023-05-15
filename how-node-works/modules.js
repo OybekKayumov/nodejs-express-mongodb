3ee2c0dbe7a21b12936b1e09e8edd4646d48a22c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-15 06:40:53 +0500

    Add modules

--- how-node-works/modules.js
+++ how-node-works/modules.js
@@ -1,4 +1,5 @@
-console.log(arguments);
+// 1
+// console.log(arguments);
 // array Arguments
 // [Arguments] {
 //   '0': {},
@@ -12,9 +13,15 @@ console.log(arguments);
 //   '4': 'folder'
 // }
 
-console.log(require('module').wrapper);
+// 2
+// console.log(require('module').wrapper);
 // [
 //   '(function (exports, require, module, __filename, __dirname) { ',  
 //   '\n});'
 // ]
 
+// 3
+const Calculator = require('./test-module-1.js');
+const calc1 = new Calculator();
+
+console.log('add: ',calc1.add(2, 5));
