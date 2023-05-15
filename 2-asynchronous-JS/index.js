34d1287025a2e17fd5a829278e23e738e50ef0ea
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-15 12:11:12 +0500

    Add dog.ceo

--- 2-asynchronous-JS/index.js
+++ 2-asynchronous-JS/index.js
@@ -1,19 +1,46 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// using call-backs inside of call-backs inside of call-backs 
+/*
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
+      if (err) return console.log(err.message);
+
       // console.log('res: ', res.body);
       console.log('res: ', res.body.message);
 
       // save img to file
       fs.writeFile('dog-img.txt', res.body.message, err => {
+        if (err) return console.log(err.message);
         console.log('Random dog image saved to file...');
       })
     });
+});
+*/
+
+// 42. From Callback Hell to Promises
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+      .then(res => {
+        if (err) return console.log(err.message);
+        console.log(res.body.message);
+
+        // save img to file
+        fs.writeFile('dog-img.txt', res.body.message, err => {
+          if (err) return console.log(err.message);
+          console.log('Random dog image saved to file...');
+        })
+      })
+      .catch(err => {
+        
+      })
 });
 
