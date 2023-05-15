50c7b4b2934bacf80db6b5905b8cdabd127b1805
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-15 05:58:56 +0500

    Add streams

--- how-node-works/streams.js
+++ how-node-works/streams.js
@@ -11,8 +11,27 @@ server.on('request', (req, res) => {
   // })
 
   // solution 2
-})
+  // const readable = fs.createReadStream("test-file.txt");
+  // readable.on('data', chunk => {
+  //   res.write(chunk);
+  // });
+  // readable.on('end', () => {
+  //   res.end();
+  // });
+  
+  // readable.on("error", err => {
+  //   console.log(err);
+  //   res.statusCode = 500;
+  //   res.end("File not found!");
+  // });
+
+  // solution 3
+  const readable = fs.createReadStream("test-file.txt");
+  readable.pipe(res);
+  // readableSource.pipe(writableDestination)
+  
+});
 
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening...');
-})
+});
