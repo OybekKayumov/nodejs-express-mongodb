72d324a77d7ee31b45d44032eb41f28d6eb09278
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-15 11:50:04 +0500

    Add dog.ceo

--- 2-asynchronous-JS/index.js
+++ 2-asynchronous-JS/index.js
@@ -7,7 +7,13 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
-      console.log('res: ', res.body);
+      // console.log('res: ', res.body);
+      console.log('res: ', res.body.message);
+
+      // save img to file
+      fs.writeFile('dog-img.txt', res.body.message, err => {
+        console.log('Random dog image saved to file...');
+      })
     });
 });
 
