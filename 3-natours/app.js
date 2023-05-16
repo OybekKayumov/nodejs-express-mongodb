b3b1a55793e485c8ba0631d7a559acb281da61a2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-16 11:57:46 +0500

    Add middleware

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+// middleware is basically function that can modify the incoming request data
+// it stands between, in the middle of the request and response
+// here data from the body is added to request object
+
+app.use(express.json());
+
 // read data and convert to array of JS object
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
@@ -16,6 +22,10 @@ app.get('/api/v1/tours', (req, res) => {
     }
   })
 });
+
+app.post('/api/v1/tours', (req, res) => {
+
+})
 
 const port = 3000;
 app.listen(port, () => {
