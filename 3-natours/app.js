ed705b9b782df498f51abc1e00ad2a11b704e29b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-16 11:03:54 +0500

    Add rest api

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -17,3 +17,31 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 })
+
+// 51. APIs and RESTful API Design
+// API - application programming interface
+  // -- web APIs
+  // -- node APIs
+  // Browser's DOM JS API
+  // with OOP created API
+
+// REST
+  // - separate API into logical resources
+  // - expose(made available using) structured, resource-based URLs
+  // - use HTTP methods(verbs), not the URL, to read, create, update, delete ... data
+  // - send data as JSON (usually)
+  // - be stateless.
+
+// CRUD operations
+
+
+// JSON 
+  // - string - value
+  // - key-value pair
+  // Object, Array
+  // response formatting
+
+// all state is handle on the client
+  // state simply refers to a piece of data in the application that might  change over time: for example, loggedIn, currentPage
+  
+
