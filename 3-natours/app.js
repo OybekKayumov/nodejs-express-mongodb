5d7f76eefe829b5d94b8b7c63f76a2380e5eef1e
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-16 07:47:31 +0500

    Add express

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     .json({message: 'Hello from the server side!', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...')
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
