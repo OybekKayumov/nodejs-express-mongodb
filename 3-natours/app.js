fee93843ea59dd2292b75d41d8813ae0ba166420
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-17 05:33:20 +0500

    Add responding to URL params

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -6,7 +6,6 @@ const app = express();
 // middleware is basically function that can modify the incoming request data
 // it stands between, in the middle of the request and response
 // here data from the body is added to request object
-
 app.use(express.json());
 
 // read data and convert to array of JS object
@@ -23,8 +22,40 @@ app.get('/api/v1/tours', (req, res) => {
   })
 });
 
-app.post('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log('req.params: ', req.params );
 
+  const id = req.params.id * 1; // convert string to number
+  const tour = tours.find(el => el.id === id)
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: tour
+    }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  // new tour
+  const newId = tours[tours.length -1].id + 1;
+  const newTour = Object.assign({id: newId}, req.body)
+
+  // push new tour into the array
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201)  // 201 created
+       .json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+       })
+  })
+
+  // res.send('Done');
 })
 
 const port = 3000;
