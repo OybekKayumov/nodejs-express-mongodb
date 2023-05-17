b93f1ab51861e17707071503fdf13b35512a77c7
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-17 06:00:01 +0500

    Add patch and delete item by id

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -72,12 +72,26 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       message: 'Invalid ID ...'
     });
   }
-  
+
   res.status(200).json({
     status: 'success',
     data: {
       tour: '<Updated tour here...>'
     }
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID ...'
+    });
+  }
+  
+  res.status(204).json({  // no-content
+    status: 'success',
+    data: null,
   })
 })
 
