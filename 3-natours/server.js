37a983553344243a7fc45bffa273f4f1af483aa1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-18 14:13:59 +0500

    Add envoronment variables

--- 3-natours/server.js
+++ 3-natours/server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
+// 67. Environment Variables
+console.log('app.get.env: ', app.get('env')); // app.get.env:  development
+console.log('process.env: ', process.env); // ...
+
 // Start Server
 const port = 3000;
 app.listen(port, () => {
