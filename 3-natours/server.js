ed9fe576262919022b9e3cd7d23cff0e523de70f
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-19 12:20:28 +0500

    Add mongodb local and cloud

--- 3-natours/server.js
+++ 3-natours/server.js
@@ -1,7 +1,44 @@
 /* eslint-disable prettier/prettier */
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+)
+
+/*
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+ 
+dbConnect().catch(err => console.log(err));
+ 
+async function dbConnect() {
+  await mongoose.connect(DB);
+}
+*/
+
+mongoose
+  // local mongodb
+  // .connect(process.env.DATABASE_LOCAL, {
+
+  .connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connections successfully !!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = require('./app');
 
