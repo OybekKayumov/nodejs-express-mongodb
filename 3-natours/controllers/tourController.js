e2ae58e091e6341f3ecf0842e5db762665a66ae2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-20 12:10:13 +0500

    Add get all tours

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -1,19 +1,26 @@
 /* eslint-disable prefer-object-spread */
 /* eslint-disable prettier/prettier */
-const fs = require('fs');
+// const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log('req.requestTime: ', req.requestTime );
-  // send back to client
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();  // return all
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    })
+  }
+
 };
 
 exports.getTour = (req, res) => {
