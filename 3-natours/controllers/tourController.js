11649597a13db8554add05f800b0dfdad8ffd5b6
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-20 12:13:17 +0500

    Add get tour by id

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -23,25 +23,22 @@ exports.getAllTours = async (req, res) => {
 
 };
 
-exports.getTour = (req, res) => {
-  console.log('req.params: ', req.params);
-  const id = req.params.id * 1; // convert string to number
-  const tour = tours.find((el) => el.id === id);
-
-  // if (id > tours.length) {
-  // if (!tour) {
-  //   return res.status(404).json({
-  //     status: 'fail',
-  //     message: 'Invalid ID...'
-  //   })
-  // }
+exports.getTour = async (req, res) => {
+ try {
+  const tour = await Tour.findById(req.params.id)
 
   res.status(200).json({
     status: 'success',
     data: {
-      tours: tour,
+      tour,
     },
   });
+ } catch (err) {
+  res.status(404).json({
+    status: 'fail',
+    message: err,
+  })
+ }
 };
 
 exports.createTour = async (req, res) => {
