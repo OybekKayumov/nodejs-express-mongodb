e7e02c6fa98e87425b8495664cfbfef053f9e9a1
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-20 14:17:34 +0500

    Add delete document

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -74,7 +74,7 @@ exports.updateTour = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour: '<Updated tour here...>',
+        tour: tour,
       },
     });
   } catch (err) {
@@ -85,10 +85,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {  
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
 
-  res.status(204).json({  // no-content
-    status: 'success',
-    data: null,
-  });
+    res.status(204).json({  // no-content
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail', 
+      message: err
+    })
+  }
 };
