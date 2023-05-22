49c1a978f784a7d39a40d19a52eb96efefa6d843
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-22 11:12:40 +0500

    Add Aggregation Pipeline: Unwinding and Projecting

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -165,7 +165,19 @@ exports.getMonthlyPlan = async (req, res) => {
   try {
     const year = req.params.year * 1;  // to Number
     
-    const plan = await Tour.aggregate([]);
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          }
+        }
+      }
+    ]);
 
     res.status(200).json({
       status: 'success',
