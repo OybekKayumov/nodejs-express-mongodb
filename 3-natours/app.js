8732f0a79eb54049d52286f406cbd3e7c821b7c8
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-23 11:44:17 +0500

    Add error class

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
