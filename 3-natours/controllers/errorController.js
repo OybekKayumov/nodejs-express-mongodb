9eb079ccd044a619c048b8073e9d640a92db5ed2
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-23 15:52:42 +0500

    Add Error handling

--- 3-natours/controllers/errorController.js
+++ 3-natours/controllers/errorController.js
@@ -1,3 +1,21 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+// eslint-disable-next-line import/no-useless-path-segments
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log('value: ', value);
+
+  const message = `Duplicate field value: ${value}. Please use another value! `;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -36,6 +54,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+
+    sendErrorProd(error, res);
   }
 };
