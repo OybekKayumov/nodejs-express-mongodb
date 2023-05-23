fcee246118ccbfed318087468dc56e507f9267ad
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-23 14:06:08 +0500

    Add Errors During Development vs Production

--- 3-natours/controllers/errorController.js
+++ 3-natours/controllers/errorController.js
@@ -1,6 +1,18 @@
 const sendErrorDev = (err, res) => {
-  
-}
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
 
 module.exports = (err, req, res, next) => {
   // console.log('err.stack: ', err.stack);
@@ -9,16 +21,8 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
+    sendErrorProd(err, res);
   }
 };
