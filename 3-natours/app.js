e80085b1ef7110ae323b22ec5d8a8c1de64de0b3
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-23 11:50:05 +0500

    Add error class

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -42,23 +43,24 @@ app.all('*', (req, res, next) => {
   //   message: `Can't find ${req.originalUrl} on this server...`,
   // })
 
-  const err = new Error(`Can't find ${req.originalUrl} on this server...`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this server...`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server...`, 404));
 });
 
 // 114. Implementing a Global Error Handling Middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+// app.use((err, req, res, next) => {
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message
+//   });
+// });
+app.use(globalErrorHandler);
 
 // 4. Start Server
 module.exports = app;
