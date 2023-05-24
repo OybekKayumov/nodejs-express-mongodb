731426ff2c222d3c6a5408d4d3a94b9a430ccef4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-24 14:35:05 +0500

    Add Signing up Users

--- 3-natours/controllers/authController.js
+++ 3-natours/controllers/authController.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-useless-path-segments */
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
@@ -11,10 +12,33 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  // create token
+  // const token = jwt.sign({ id: newUser._id }, 'secret', expire_time);
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    // send token to client
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+// {
+//   "status": "success",
+//   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NmRkOTIxOTMwZjlmOWUyOGNjZDdlMyIsImlhdCI6MTY4NDkyMDYwOSwiZXhwIjoxNjkyNjk2NjA5fQ.HPws6KnQKuRmkAIAjS2XuAlIk54vxYYseVr0hRjmbnI",
+//   "data": {
+//       "user": {
+//           "name": "john1",
+//           "email": "john1@mail.com",
+//           "password": "$2a$12$7wiMYh0rQoaz/LSP6TSAtuzthN0XLH3O2/v/f0z8ihRwDzimSbgYq",
+//           "_id": "646dd921930f9f9e28ccd7e3",
+//            "id": "646dd921930f9f9e28ccd7e3" //! from jwt.io
+//           "__v": 0
+//       }
+//   }
+// }
