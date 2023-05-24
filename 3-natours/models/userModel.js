63ab2c88fc0b21a8f4af96d1237ff32482051610
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-24 11:55:22 +0500

    Add Modelling Users

---
+++ 3-natours/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// name, email, photo, password, password confirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    // check for correct email address
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
