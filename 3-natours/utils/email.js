a3d925b94403c1771251818a60a7fb40208e27bd
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-25 20:15:59 +0500

    Add Sending Emails with Nodemailer

--- 3-natours/utils/email.js
+++ 3-natours/utils/email.js
@@ -1,17 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const nodemailer = require('nodemailer');
 
-const sendEmail = options => {
+const sendEmail = async (options) => {
   // create a transporter
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    // service: 'Gmail',
+    service: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
+
     // activate in gmail 'less secure app' option
-    
   });
   // define the email options
+  const mailOptions = {
+    from: 'John Doe <hello@mails.io>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
 
   // send email with nodemailer
+  await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;
