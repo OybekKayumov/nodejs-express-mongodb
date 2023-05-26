0c1bda4305ab96054077029915566acd4c9b2412
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-26 11:49:14 +0500

    Add Implementing Rate Limiting

--- 3-natours/app-143.js
+++ 3-natours/app-143.js
@@ -31,6 +31,12 @@ app.use(express.json());
 // Serving Static Files
 app.use(express.static(`${__dirname}/public`))
 
+// app.use((req, res, next) => {
+//   console.log('Hello from middleware!');
+
+//   next();  // !
+// })
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
@@ -44,12 +50,29 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 //! put after real routes, order is important
-app.all('*', (req, res, next) => {  
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server...`,
+  // })
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server...`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
   next(new AppError(`Can't find ${req.originalUrl} on this server...`, 404));
 });
 
 // 114. Implementing a Global Error Handling Middleware
+// app.use((err, req, res, next) => {
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
+
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message
+//   });
+// });
 app.use(globalErrorHandler);
 
 // 4. Start Server
