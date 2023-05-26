f4c7eac7a0997e7d755975610097dd0bfa9fb43c
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-26 18:36:34 +0500

    Add Data Sanitization

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +35,13 @@ app.use('/api', limiter);
 
 // body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSQL query injection
+//Login as: "email": { "$gt": ""}. "password": "password"
+app.use(mongoSanitize());
+
+// data sanitization against XSS, some malicious HTML code
+app.use(xss());
 
 // Serving Static Files
 app.use(express.static(`${__dirname}/public`))
