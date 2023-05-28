1f383b8827a95348f86f0ef2c0b5736b21e98261
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-28 11:22:39 +0500

    Add Creating and Getting Reviews

--- 3-natours/routes/reviewRoutes.js
+++ 3-natours/routes/reviewRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.route('/review')
+router.route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.protect,
+    authController.restrictTo('user'),
+    reviewController.createReview
+  );
 
 module.exports = router;
