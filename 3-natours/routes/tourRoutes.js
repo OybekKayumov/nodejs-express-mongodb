458eddff28cb8578b68b505642a9642bc79b7a09
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-28 12:54:52 +0500

    Add Implementing Simple Nested Routes

--- 3-natours/routes/tourRoutes.js
+++ 3-natours/routes/tourRoutes.js
@@ -42,7 +42,7 @@ router
   .route('/:tourId/reviews')
   .post(
     authController.protect,
-    authController.restrictTo('users'),
+    authController.restrictTo('user'),
     reviewController.createReview
   );
 
