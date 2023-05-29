0e495844e2dfa8661abcd35a33d6afd5432632e4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-29 05:08:18 +0500

    Add Building Handler Factory Functions: Delete

---
+++ 3-natours/controllers/handlerFactory.js
@@ -0,0 +1,29 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+
+// exports.deleteTour = catchAsync(async (req, res, next) => {
+//   const tour = await Tour.findByIdAndDelete(req.params.id);
+
+//   if (!tour) {
+//     return next(new AppError('No tour found with that ID', 404));
+//   }
+
+//   res.status(204).json({  // no-content
+//     status: 'success',
+//     data: null,
+//   });
+// });
