8a12e50b2d047e4b1e2e94718852516b83e01e82
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-29 05:30:56 +0500

    Add Factory Functions: Update and Create

--- 3-natours/controllers/userController.js
+++ 3-natours/controllers/userController.js
@@ -83,12 +83,14 @@ exports.createUser = (req, res) => {
   });
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
+// exports.updateUser = (req, res) => {
+//   res.status(500).json({
+//     status: 'error',
+//     message: 'This route is not yet defined',
+//   });
+// };
+//! do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
 
 // exports.deleteUser = (req, res) => {
 //   res.status(500).json({
