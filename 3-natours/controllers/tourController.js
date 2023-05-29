02c6e6a9cad43857344c039bf77ade34e5cdd473
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-29 07:06:25 +0500

    Add Factory Functions: Reading

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -3,7 +3,6 @@
 /* eslint-disable prefer-object-spread */
 /* eslint-disable prettier/prettier */
 const Tour = require('./../models/tourModel');
-// const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
@@ -16,98 +15,11 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-// exports.getAllTours = catchAsync(async (req, res, next) => {
-//   //TODO: execute query
-//   const features = new APIFeatures(Tour.find(), req.query)
-//     .filter()
-//     .sort()
-//     .limitFields()
-//     .paginate();
-//   const tours = await features.query;
 
-//   //TODO: send response
-//   res.status(200).json({
-//     status: 'success',
-//     results: tours.length,
-//     data: {
-//       tours,
-//     },
-//   });
-// });
 exports.getAllTours = factory.getAll(Tour);
-
-// exports.getTour = catchAsync(async (req, res, next) => {
-//   // find one item
-//   const tour = await Tour.findById(req.params.id).populate('reviews');
-//   //   .populate({
-//   //   path: 'guides}',
-//   //   select: '-__v -passwordChangedAt' // show data without this 2 fields
-//   // }); 
-//                  // Tour.findOne({ _id: req.params.id }) 
-  
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   } 
-  
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour,
-//     },
-//   }); 
-// });
 exports.getTour = factory.getOne(Tour, { path: 'reviews' });
-
-// const catchAsync = fn => {
-//   return (req, res, next) => {
-//     // fn(req, res, next).catch(err => next(err));
-//     fn(req, res, next).catch(next);
-//   };
-// };
-
-// exports.createTour = catchAsync(async (req, res, next) => {
-//   const newTour = await Tour.create(req.body);
-
-//   res.status(201).json({
-//       status: 'success',
-//       data: {
-//         tour: newTour,
-//       },
-//     }); 
-// });
 exports.createTour = factory.createOne(Tour);
-
-// exports.updateTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//     runValidators: true   // !
-//   });
-
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   } 
-  
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour,
-//     },
-//   });
-// });
 exports.updateTour = factory.updateOne(Tour);
-
-// exports.deleteTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   } 
-  
-//   res.status(204).json({  // no-content
-//     status: 'success',
-//     data: null,
-//   });
-// });
 exports.deleteTour = factory.deleteOne(Tour);
 
 // 102. Aggregation Pipeline: Matching and Grouping
--- 3-natours/controllers/userController-163.js
+++ 3-natours/controllers/userController-163.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-useless-path-segments */
-const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 const filterObj = (obj, ...allowedFields) => {
@@ -13,6 +13,24 @@ const filterObj = (obj, ...allowedFields) => {
 
   return newObj;
 };
+
+// exports.getAllUsers = catchAsync(async (req, res, next) => {
+// res.status(500).json({
+//   status: 'error...',
+//   message: 'This route is not defined yet'
+// });
+//   const users = await User.find();
+
+//   //TODO: send response
+//   res.status(200).json({
+//     status: 'success',
+//     results: users.length,
+//     data: {
+//       users,
+//     },
+//   });
+// });
+exports.getAllUsers = factory.getAll(User);
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // create error if user POSTs password data
@@ -52,6 +70,14 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
+// exports.getUser = (req, res) => {
+//   res.status(500).json({
+//     status: 'error',
+//     message: 'This route is not yet defined',
+//   });
+// };
+exports.getUser = factory.getOne(User);
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
@@ -59,8 +85,19 @@ exports.createUser = (req, res) => {
   });
 };
 
-exports.getAllUsers = factory.getAll(User);
-exports.getUser = factory.getOne(User);
+// exports.updateUser = (req, res) => {
+//   res.status(500).json({
+//     status: 'error',
+//     message: 'This route is not yet defined',
+//   });
+// };
 //! do NOT update passwords with this!
 exports.updateUser = factory.updateOne(User);
+
+// exports.deleteUser = (req, res) => {
+//   res.status(500).json({
+//     status: 'error',
+//     message: 'This route is not yet defined',
+//   });
+// };
 exports.deleteUser = factory.deleteOne(User);
