46cd2f123e42422ed1ada7d1e3c97e5b75056dde
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-29 12:07:22 +0500

    Add Missing Authentication and Authorization

--- 3-natours/routes/reviewRoutes.js
+++ 3-natours/routes/reviewRoutes.js
@@ -5,11 +5,13 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+// access only after authenticating
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
