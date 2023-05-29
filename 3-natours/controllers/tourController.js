e111c719001f31b1cfadee0f3e7ce52a05ae461b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-29 05:14:34 +0500

    Add Building Handler Factory Functions: Delete

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -5,6 +5,7 @@ const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -90,18 +91,19 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
+// exports.deleteTour = catchAsync(async (req, res, next) => {
+//   const tour = await Tour.findByIdAndDelete(req.params.id);
 
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  } 
+//   if (!tour) {
+//     return next(new AppError('No tour found with that ID', 404));
+//   } 
   
-  res.status(204).json({  // no-content
-    status: 'success',
-    data: null,
-  });
-});
+//   res.status(204).json({  // no-content
+//     status: 'success',
+//     data: null,
+//   });
+// });
+exports.deleteTour = factory.deleteOne(Tour);
 
 // 102. Aggregation Pipeline: Matching and Grouping
 exports.getTourStats = catchAsync(async (req, res, next) => {
