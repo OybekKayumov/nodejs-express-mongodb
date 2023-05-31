dc684ea4256027140fa3b4eaae9a3dfafdaf5609
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-05-31 09:54:57 +0500

    Add Building the Tour Overview

--- 3-natours/controllers/viewsController.js
+++ 3-natours/controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1 get tour data from collection
+  
+  // 2 build template
+  // 3 render template using tour data from 1
+
   res.status(200).render('overview', {
     title: 'All Tours',
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
