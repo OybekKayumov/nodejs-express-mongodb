5933d4058a8615df94b89e44b710615d7e52a0f4
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-04 05:13:01 +0500

    Add multer

--- 3-natours/controllers/userController.js
+++ 3-natours/controllers/userController.js
@@ -8,8 +8,21 @@ const factory = require('./handlerFactory');
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'public/img/users');
-  }
+  },
+  filename: (req, file, cb) => {
+    // user-id-timestamp.jpeg
+    const ext = file.mimetype.split('/')[1]; // 'image/jpeg'
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
 });
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+}
 
 const upload = multer({ dest: 'public/img/users' });
 
