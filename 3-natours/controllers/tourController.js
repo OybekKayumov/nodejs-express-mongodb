ae25de2e951d6540ed57fc135e24aa5b86d5d75f
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-05 07:22:50 +0500

    Add Processing Multiple Images

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -33,10 +33,22 @@ exports.uploadTourImages = upload.fields([
 // upload.single('image')      req.file
 // upload.array('images', 5);  req.files
 
-exports.resizeTourImages = (req, res, next) => {
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
 
+  // cover image
+  const imageCoverFilename = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${imageCoverFilename}`);
+
+  req.body.imageCover = imageCoverFilename;
+  
+  // images
   next();
-};
+});
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
