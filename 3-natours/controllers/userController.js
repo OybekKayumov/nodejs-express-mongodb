3f890fd0222b12b66736cf955753f697532ec764
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-05 05:37:08 +0500

    Add Resizing Images

--- 3-natours/controllers/userController.js
+++ 3-natours/controllers/userController.js
@@ -38,7 +38,15 @@ exports.uploadUserPhoto = upload.single('photo');
 exports.resizeUserPhoto = (req, res, next) => {
   if (!req.file) return next();
 
-  sharp(req.file.buffer).resize(500, 500);
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
 };
 
 const filterObj = (obj, ...allowedFields) => {
