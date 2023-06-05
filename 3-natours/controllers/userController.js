9a3700036f2d7cfc73f466058932278b496ed0dc
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-05 05:32:53 +0500

    Add Resizing Images

--- 3-natours/controllers/userController.js
+++ 3-natours/controllers/userController.js
@@ -1,20 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-useless-path-segments */
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    // user-id-timestamp.jpeg
-    const ext = file.mimetype.split('/')[1]; // 'image/jpeg'
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     // user-id-timestamp.jpeg
+//     const ext = file.mimetype.split('/')[1]; // 'image/jpeg'
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   },
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -31,6 +34,12 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  sharp(req.file.buffer).resize(500, 500);
+};
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
