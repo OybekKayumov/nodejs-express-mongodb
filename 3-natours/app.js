83e3d5de21d105e036d5d34c71fdc1273290f875
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-06-09 15:35:46 +0500

    Add cors

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -11,6 +11,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +30,8 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));  // /views
 
 // 1. Global Middlewares
+app.use(cors());
+
 // Serving Static Files
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
